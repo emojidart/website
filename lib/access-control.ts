f35dacf2590c19f024d9e254732c1f0b2fd08e6f
emojidart <emojidart@gmail.com>
2026-08-16 17:49:49 +0200

    Lion Cup Regelwerk öffentlich freigeben

--- lib/access-control.ts
+++ lib/access-control.ts
@@ -10,7 +10,7 @@ export const publicRoutes = [
   "/gastzugang",
   "/guest-login",
   "/member-login",
-
+  "/lion-cup-regelwerk",
   "/veranstaltungen",
   "/liga-statistiken-app",
   "/new-club",
